fa48d24b1ec2851abf13d792b08882ebbc432757
Shoaib Qureshi <shoaib@d3vtech.com>
2024-11-14 12:29:57 +0530

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,16 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+          $user = auth()->user();
+        $data = $request->validated();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($data['password']);
+        }
+
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
